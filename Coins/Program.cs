ea71e186988920de3fe30e2c2d7b4a0700f841b1
Ondřej Novosád <novosad.ondrej.2017@skola.ssps.cz>
2021-03-25 19:28:44 +0100

    Add project files.

---
+++ Coins/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Coins
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kolik peněz musíte zaplatit?: ");
+            int price = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Kolika penězi platíte?: ");
+            int cash = Convert.ToInt32(Console.ReadLine());
+            int result = cash - price;
+
+            int[] coins = { 50, 20, 10, 5, 2, 1 };
+            for (int i = 0; i < coins.Length; i++)
+            {
+                int count = result / coins[i];
+                if (count != 0)
+                {
+                    Console.WriteLine($"Vrátíme Vám {count}x {coins[i]}Kč");
+                }
+                result %= coins[i];
+            }
+
+        }
+
+    }
+}
